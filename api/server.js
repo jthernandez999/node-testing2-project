3ce6378cd9f4a68b0e5e58f3e719ae7c6fbab43b
Joe Hernandez <j.t.hernandez999@gmail.com>
2021-12-09 19:19:32 -0800

    wip

--- api/server.js
+++ api/server.js
@@ -1,3 +1,15 @@
 const express = require('express')
 
-const Offices = require('')
+const Offices = require('./offices/offices-model')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'up'})
+})
+
+
+
+module.exports = server;
